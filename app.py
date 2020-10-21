61e15e2ebf0a925838b7afb65fe13c574894d876
mclb3720 <mclb3720@mailhost.cs.uleth.ca>
2020-10-21 12:20:37 -0600

    Sessions. Login/Logout. Message Flashing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, url_for, render_template, request
-from markupsafe import escape
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 app = Flask(__name__)
+
+app.secret_key = "Super secret key!!!"
 
 @app.route('/')
 def home():
@@ -10,17 +11,29 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form["name"]
-        return redirect(url_for("show_user", name=user))
+        session["user"] = user;
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route('/users')
-def show_all_users():
-    return 'All Users'
+@app.route('/logout')
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
+        session.pop("user", None)
+    return redirect(url_for("home"))   
 
-@app.route('/users/<name>')
-def show_user(name):
-    return f"<h1>{name}</h1>"
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        return redirect(url_for("login"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
