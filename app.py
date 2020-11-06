a2fd0581c4c83e9d52acbe6b327355ad29378f07
Blake McLachlin <blake.mclachlin@uleth.ca>
2020-11-06 11:39:08 -0700

    Use mysql db for logging in

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+import mysql.connector
+from mysql.connector import errorcode
+from datetime import date, datetime, timedelta
+import sys
+
 app = Flask(__name__)
 
 app.secret_key = "Super secret key!!!"
+
+# Connect to 'demo' DB as 'blake'
+try:
+  cnx = mysql.connector.connect(user='blake', password='password', host='127.0.0.1', database = 'demo')
+  cnx.autocommit = True
+  cursor = cnx.cursor(dictionary=True)
+  print("Successfully connected to demo!")
+except mysql.connector.Error as err:
+  print(f"Error: {err}")
+  sys.exit(0)
 
 @app.route('/')
 def home():
@@ -9,31 +24,51 @@ def home():
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        user = request.form["name"]
-        session["user"] = user;
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            return redirect(url_for("user"))
-        return render_template("login.html")
+    # An output message
+    msg=''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # store username and password in variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        # query the database for a user with username and password
+        query = ('SELECT * FROM users WHERE username=%s AND password=%s')
+        data_user = (username, password)
+        cursor.execute(query, data_user)
+
+        # grab record from database
+        account = cursor.fetchone()
+
+        # if account exists
+        if account:
+            session['loggedin'] = True
+            session['username'] = account['username']
+            
+            # redirect the user to the user page
+            return redirect(url_for('user'))
+        # account does not exist
+        else:
+            msg='Incorrect username and/or password!'
+    
+    return render_template("login.html", msg=msg)
 
 @app.route('/logout')
 def logout():
-    if "user" in session:
-        user = session["user"]
+    if "username" in session:
+        user = session["username"]
         flash(f"You have been logged out, {user}", "info")
-        session.pop("user", None)
+        session.pop("username", None)
+        session['loggedin'] = False
     return redirect(url_for("home"))   
 
 @app.route('/user')
 def user():
-    if "user" in session:
-        user = session["user"]
+    if "username" in session and session['loggedin'] == True:
+        user = session["username"]
         return render_template("user.html", user=user)
     else:
         return redirect(url_for("login"))
-
-
+        
 if __name__ == '__main__':
     app.run(debug=True)
